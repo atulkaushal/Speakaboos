54289482bf75025dce4ea69c31130daa2291870b
Suresh Kada <Suresh.Kada@gmail.com>
2022-05-12 10:30:24 -0400

    Initial commit

---
+++ src/main/java/com/wfhackathon2022/speakaboos/SpeakaboosApplication.java
@@ -0,0 +1,13 @@
+package com.wfhackathon2022.speakaboos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeakaboosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeakaboosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wfhackathon2022/speakaboos/SpeakaboosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wfhackathon2022.speakaboos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeakaboosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
