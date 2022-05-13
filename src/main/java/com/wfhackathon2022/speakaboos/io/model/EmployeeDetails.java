63f7edc91cece5dda1e753487f0b78f1b5240d03
gmaheshwari2006 <32501070+gmaheshwari2006@users.noreply.github.com>
2022-05-13 13:58:07 -0400

    removed extra annotations

--- src/main/java/com/wfhackathon2022/speakaboos/io/model/EmployeeDetails.java
+++ src/main/java/com/wfhackathon2022/speakaboos/io/model/EmployeeDetails.java
@@ -1,7 +1,5 @@
 package com.wfhackathon2022.speakaboos.io.model;
 
-import javax.persistence.Column;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Getter;
@@ -14,15 +12,12 @@ public class EmployeeDetails {
 	private Integer employeeId;
 	
 	@Getter @Setter
-	@Column(name = "legalFirstName")
 	private String legalFirstName;
 	
 	@Getter @Setter
-	@Column(name = "legalLastName")
 	private String legalLastName;
 	
 	@Getter @Setter
-	@Column(name = "preferredName")
 	private String preferredName;
 	
 }
