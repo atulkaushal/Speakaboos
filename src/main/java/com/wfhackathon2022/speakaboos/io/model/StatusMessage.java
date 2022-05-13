ff67b79b7d69b034deee06843d597df009aa71e6
Suresh Kada <Suresh.Kada@gmail.com>
2022-05-13 11:12:59 -0400

    getEmployeeDetails, error, status

---
+++ src/main/java/com/wfhackathon2022/speakaboos/io/model/StatusMessage.java
@@ -0,0 +1,42 @@
+package com.wfhackathon2022.speakaboos.io.model;
+
+import org.springframework.validation.annotation.Validated;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Validated
+@EqualsAndHashCode
+public class StatusMessage {
+
+	public enum TypeEnum {
+		INFOORMATION("INFORMATION"),
+		WARNING("WARNING"),
+		ERROR("ERROR");
+		
+		private String value;
+		
+		TypeEnum(String value){
+			this.value = value;
+		}
+
+		public String getValue() {
+			return value;
+		}
+	}
+	
+	@JsonProperty("type")
+	@Getter @Setter
+	private String type = null;
+	
+	@JsonProperty("code")
+	@Getter @Setter
+	private String code = null;
+	
+	@JsonProperty("message")
+	@Getter @Setter
+	private String message = null;
+}
